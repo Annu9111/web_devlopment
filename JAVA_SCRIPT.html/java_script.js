ea4b3760f29c05bdc235ae8bad635baf8fc673ac
Annu9111 <aksoni911190@gmail.com>
2026-01-16 20:04:08 +0530

    function in javascript

---
+++ JAVA_SCRIPT.html/java_script.js
@@ -0,0 +1,21 @@
+console.log("this is java_script file");
+
+let names="annu";
+console.log(names);
+
+function greet(names){                   //all the  parameter and argument is similar to python
+    console.log(names + " welcome!!!!"); 
+}
+let name1="annu";
+greet(name1);
+
+function sum(a,b,c){
+    let nums=a+b+c;
+    return nums
+}
+
+let return_val=sum(3,4,5);
+console.log(return_val);
+
+
+
