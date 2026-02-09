a48b079ca7bb43bbf9badaf597e8afad6b34ddf6
Annu9111 <aksoni911190@gmail.com>
2026-02-09 17:23:34 +0530

    pug

--- JAVA_SCRIPT.html/Express/Aap.js
+++ JAVA_SCRIPT.html/Express/Aap.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const path=require("path");
 
 const app = express();
 const port=80;
 
+app.use('/statics',express.static('statics'))
+app.set('view engine','pug')
+app.set('views',path.join(__dirname,'views'))
+
+app.get("/demo",(req,res)=>{
+    res.status(200).render('demo', { title: 'Hey', message: 'Hello there!' })
+});
 app.get("/",(req,res)=>{
     res.send("this is my first request App")
 });
